b8216d9ff1d60587502fa0d90e18dbd233773d06
Radhika <rg583@njit.edu>
2018-12-15 13:43:29 -0500

    Fix : Made changes to EmailVerificationMailable file

--- app/Mail/EmailVerificationMailable.php
+++ app/Mail/EmailVerificationMailable.php
@@ -10,15 +10,15 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class EmailVerificationMailable extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $user;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -28,6 +28,6 @@ class EmailVerificationMailable extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('mails.verifyEmailFirst');
     }
 }
